2b6daf8d4b035f78ac77c54b5466a532331b2b7a
jzane <julianzane1@gmail.com>
2017-09-22 14:57:50 -0400

    Fixed countGL_10
    Rewrote the function

--- countGL_10.m
+++ countGL_10.m
@@ -1,19 +1,35 @@
-function v = countGL_10( im )
-%UNTITLED Summary of this function goes here
+function v = countGL_10( im ) %cannot seem to name function something other than filename
+% UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 v=zeros(256,1);
-    if(nargin<1)
+
+    if(nargin<1) % nargin returns the number of function inputs
         disp("Please specify image matrix");
         v=[];
         return;
     end
-    if(isinteger(im)~=true)
+    
+    if(isinteger(im)~=true) % if false
         disp("“Image matrix must be of type uint8");
         v=[];
         return;
     end
-    for i=1:1:256
-        v(i)=nnz(im==i-1);
+    
+%     for i=1:1:256 % creating a 1x256 vector
+%         v(i)=nnz(im==i-1);
+%     end
+
+
+    % Initialize vector v to zero
+    v = zeros([1 256]);
+    % Iterate through each column and row and populate V
+    for row=1:size(im,1) % size(im, 1) -> number of rows
+        for col=1:size(im,2) % size(im, 2) -> number of columns 
+            grayLevel = im(row, col); % tis might be of type double
+            grayLevelInt = uint8(grayLevel); % convert to uint8 if not already
+            v(grayLevelInt + 1) = v(grayLevelInt + 1) + 1; % Increment the number in v
+        end
     end
+    
 end
 
